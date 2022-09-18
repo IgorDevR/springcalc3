8683513ea7a9dc8e5181a9cbc7c152d52d3b4fbe
Igor <47169805+IgorDevR@users.noreply.github.com>
2022-09-18 12:29:15 +0300

    upd

--- src/main/java/comskyprocalcspringcalc/CalcController.java
+++ src/main/java/comskyprocalcspringcalc/CalcController.java
@@ -9,35 +9,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/calculator")
 public class CalcController {
 
-    private final CalcServiceImp calcServiceImp;
+	private final CalcService calcService;
 
-    public CalcController(CalcServiceImp calcServiceImp) {
-        this.calcServiceImp = calcServiceImp;
-    }
+	public CalcController(CalcService calcService) {
+		this.calcService = calcService;
+	}
 
-    @GetMapping()
-    public String greetings() {
-        return calcServiceImp.calcGreetings();
-    }
+	@GetMapping()
+	public String greetings() {
+		return calcService.calcGreetings();
+	}
 
-    @GetMapping(path = "/plus")
-    public String calcAddition(@RequestParam double num1, double num2) {
-        return String.format("%s + %s = %.2f", num1, num2, calcServiceImp.calcAddition(num1, num2));
-    }
+	@GetMapping(path = "/plus")
+	public String calcAddition(@RequestParam double num1, @RequestParam double num2) {
+		return String.format("%s + %s = %.2f", num1, num2, calcService.calcAddition(num1, num2));
+	}
 
-    @GetMapping(path = "/minus")
-    public String calcSubtraction(@RequestParam int num1, int num2) {
-        return String.format("%s - %s = %.2f", num1, num2, calcServiceImp.calcSubtraction(num1, num2));
-    }
+	@GetMapping(path = "/minus")
+	public String calcSubtraction(@RequestParam double num1, @RequestParam double num2) {
+		return String.format("%s - %s = %.2f", num1, num2, calcService.calcSubtraction(num1, num2));
+	}
 
-    @GetMapping(path = "/multiply")
-    public String calcMultiply(@RequestParam int num1, int num2) {
-        return String.format("%s * %s = %.2f", num1, num2, calcServiceImp.calcMultiply(num1, num2));
-    }
+	@GetMapping(path = "/multiply")
+	public String calcMultiply(@RequestParam double num1, @RequestParam double num2) {
+		return String.format("%s * %s = %.2f", num1, num2, calcService.calcMultiply(num1, num2));
+	}
 
-    @GetMapping(path = "/divide")
-    public String calcDivide(@RequestParam int num1, int num2) {
-        return String.format("%s / %s = %.2f", num1, num2, calcServiceImp.calcDivide(num1, num2));
-    }
+	@GetMapping(path = "/divide")
+	public String calcDivide(@RequestParam double num1, @RequestParam double num2) {
+		return num2 == 0 ? "Деление на 0" : String.format("%s / %s = %.2f", num1, num2, calcService.calcDivide(num1, num2));
+	}
 
 }
