9ef6f3fe8933a465f52953f9970f65f262b200d2
Igor <47169805+IgorDevR@users.noreply.github.com>
2022-09-20 16:05:38 +0300

    2.5

---
+++ src/main/java/collection/com/EmployeeService.java
@@ -0,0 +1,14 @@
+package collection.com;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+
+    Employee addEmployee(String firstName, String lastName);
+    Employee removeEmployee(String firstName, String lastName);
+    Employee findEmployee(String firstName, String lastName);
+
+
+
+}
